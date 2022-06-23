951322ca195fb271067cef4b5d5fab183ab09218
Afghan <afghanekapangestu@gmail.com>
2022-06-23 18:35:42 +0700

    Change read url

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ st.write('Mempertahankan brand "canada goose" agar tetap menjadi penjualan terti
 st.markdown('# All Data')
 @st.cache
 def load_csv_data():
-    data = pd.read_csv('Final_Data_Sales.csv')  # gives TextFileReader
-#     data = pd.concat(tp, ignore_index=True)
+    data = pd.read_csv('https://media.githubusercontent.com/media/afg2002/canada-goose/main/Final_Data_Sales.csv')  # gives TextFileReader
 
-#     # Convert data yang bukan datetime yang seperti 0000-0000 ke Datetime agar hasilnya NaT
-#     data['sold_at'] = pd.to_datetime(data['sold_at'], errors='coerce')
-#     data['created_at'] = pd.to_datetime(data['created_at'], errors='coerce')
-#     data['shipped_at'] = pd.to_datetime(data['shipped_at'], errors='coerce')
-#     data['delivered_at'] = pd.to_datetime(data['delivered_at'], errors='coerce')
-#     data['returned_at'] = pd.to_datetime(data['returned_at'], errors='coerce')
+    # Convert data yang bukan datetime yang seperti 0000-0000 ke Datetime agar hasilnya NaT
+    data['sold_at'] = pd.to_datetime(data['sold_at'], errors='coerce')
+    data['created_at'] = pd.to_datetime(data['created_at'], errors='coerce')
+    data['shipped_at'] = pd.to_datetime(data['shipped_at'], errors='coerce')
+    data['delivered_at'] = pd.to_datetime(data['delivered_at'], errors='coerce')
+    data['returned_at'] = pd.to_datetime(data['returned_at'], errors='coerce')
 
     # Ambil data date dari data setelahnya.
     data.fillna(method='bfill',inplace=True)
